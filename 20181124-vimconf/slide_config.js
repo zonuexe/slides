3ce52a9f7c7af3c573eda87f387d1c792b1c01c4
USAMI Kenta <tadsan@zonu.me>
2018-10-25 02:17:26 +0900

    Add Vim (WIP)

---
+++ 20181124-vimconf/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Modern editor-independent development environment for PHP', 
+     subtitle: '', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/emacs-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Kenta Usami @tadsan', 
+     company: 'pixiv Inc.', 
+     gplus: '', 
+     twitter: '@tadsan', 
+     www: 'https://tadsan.github.io', 
+     github: 'https://github.com/zonuexe', 
+   }]
+};
